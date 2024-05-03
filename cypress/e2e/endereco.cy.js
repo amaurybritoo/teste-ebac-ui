e60350ee9244e69a71eb48da6096d8abd542303b
Amaury Brito <amaurypb845@gmail.com>
2024-05-02 20:32:25 -0400

    page-objects

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,15 +1,37 @@
 /// <reference types="cypress"/>
 
+import enderecoPage from '../support/page-obects/endereco.page';
+
 const perfil = require('../fixtures/perfil.json')
 
+//usando a lib faker
+var faker = require('@faker-js/faker')
+
+//criando as constantes fakes
+const nome = faker.fakerPT_BR.person.firstName("male")
+const sobrenome = faker.fakerPT_BR.person.lastName("male")
+const email = faker.fakerPT_BR.internet.email(nome, sobrenome)
+const estado = faker.fakerPT_BR.location.state()
+const numero = faker.fakerPT_BR.number.int(53)
+const cidade = faker.fakerPT_BR.location.city()
+const rua = faker.fakerPT_PT.location.street()
+const cel = faker.fakerPT_BR.phone.number()
+
 describe('Funcionalidade Endereço - Faturamento e Entrega', () => {
-    
+
     beforeEach(() => {
+        //Entra na aba da minha conta e preenche com login e senha 
         cy.visit('minha conta')
-        cy.login(perfil.usuario,perfil.senha)
+        cy.login(perfil.usuario, perfil.senha)
+        //entra direto na aba de Endereço
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
     });
-    
+
     it('Deve fazer o cadastro de faturamento', () => {
-        
+        enderecoPage.editarEnderecoFaturamento(nome,sobrenome,"EBAC","Brasil",rua, numero, cidade,estado, '69005-330', cel,email)
+
+        //resultado esperado
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
+
     });
 });
---
+++ cypress/support/page-obects/endereco.page.js
@@ -0,0 +1,32 @@
+class EnderecoPage {
+
+    editarEnderecoFaturamento(nome, sobrenome, empresa, pais, rua, numero, cidade, estado, cep, cel, email) {
+        //entrando no Editar do Endereço de Faturamento
+        cy.get(':nth-child(1) > .title > .edit').click()
+
+        //preenchendo nomes
+        cy.get('#billing_first_name').clear().type(nome)
+        cy.get('#billing_last_name').clear().type(sobrenome)
+        cy.get('#billing_company').clear().type(empresa)
+
+        //preenchendo informações de localidade
+        cy.get('#select2-billing_country-container').click().type(pais).get('[aria-selected="true"]').click()
+        cy.get('#billing_address_1').clear().type(rua)
+        cy.get('#billing_address_2').clear().type(numero)
+        cy.get('#billing_city').clear().type(cidade)
+        cy.get('#select2-billing_state-container').click().type(estado + '{enter}')
+        cy.get('#billing_postcode').clear().type(cep)
+
+        //preenchendo informações de contato
+        cy.get('#billing_phone').clear().type(cel)
+        cy.get('#billing_email').clear().type(email)
+        cy.get('.button').click()
+
+    }
+
+    editarEnderecoEntrega() {
+
+    }
+}
+
+export default new EnderecoPage() 
