8a176d4404b0bccd95ece418404ef4c09af206de
Amaury Brito <amaurypb845@gmail.com>
2024-05-04 01:25:11 -0400

    exercicio

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -4,6 +4,8 @@ import enderecoPage from '../support/page-obects/endereco.page';
 
 const perfil = require('../fixtures/perfil.json')
 
+const dadosEndereco = require('../fixtures/enderecoFaturamento.json')
+
 //usando a lib faker
 var faker = require('@faker-js/faker')
 
@@ -27,11 +29,52 @@ describe('Funcionalidade Endereço - Faturamento e Entrega', () => {
         cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
     });
 
-    it('Deve fazer o cadastro de faturamento', () => {
-        enderecoPage.editarEnderecoFaturamento(nome,sobrenome,"EBAC","Brasil",rua, numero, cidade,estado, '69005-330', cel,email)
+    it('Deve fazer o cadastro de faturamento - usando parametros FAKES', () => {
+        enderecoPage.editarEnderecoFaturamento(nome, sobrenome, "EBAC", "Brasil", rua, numero, cidade, estado, '69005-330', cel, email)
 
         //resultado esperado
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
+
+    });
+
+    it('Deve fazer o cadastro de faturamento - Usando a massa de dados', () => {
+        enderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[0].nome,
+            dadosEndereco[0].sobrenome,
+            dadosEndereco[0].empresa,
+            dadosEndereco[0].pais,
+            dadosEndereco[0].rua,
+            dadosEndereco[0].numero,
+            dadosEndereco[0].cidade,
+            dadosEndereco[0].estado,
+            dadosEndereco[0].cep,
+            dadosEndereco[0].cel,
+            dadosEndereco[0].email)
+
+        //resultado esperado
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
+
+    });
+
+    it.only('Deve fazer o cadastro de Endereço para envio', () => {
+        //exercicio
+        //criar os metodos e ações no page object
+        //criar a massa de dados
+        enderecoPage.editarEnderecoEntrega(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].rua,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep
+
+        )
+
+        //Resultado esperado
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
 
     });
 });
